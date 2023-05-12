66561656af843c2922138db4bfe8e291dc35612b
id2k1149 <id2k1149@gmail.com>
2023-05-12 11:36:53 -0400

    initial code

--- CircleWithMarks/ContentView.swift
+++ CircleWithMarks/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            CircleView()
         }
-        .padding()
+    }
+    
+    struct CircleView: View {
+        var body: some View {
+            let circleDiameter = UIScreen.main.bounds.width * 0.9
+           
+            Circle()
+//                .fill(Color.yellow)
+                .stroke(lineWidth: 2)
+                .frame(width: circleDiameter,
+                       height: circleDiameter)
+            
+            ForEach(0..<5, id: \.self) { iteration in
+                Mark(circleDiameter: circleDiameter)
+                    .rotationEffect(.degrees(Double(iteration) * 360 / 5))
+            }
+        }
+    }
+    
+    struct Mark: View {
+        var circleDiameter: CGFloat
+        
+        var body: some View {
+            Rectangle()
+                .fill(Color.black)
+                .frame(width: 2,
+                       height: 20)
+                .offset(y: -circleDiameter / 2)
+        }
     }
 }
 
