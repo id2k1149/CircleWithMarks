58ba151bd9c6ff4762abcfc19d087526d48c4b32
id2k1149 <id2k1149@gmail.com>
2023-05-12 10:13:49 -0400

    Initial Commit

---
+++ CircleWithMarks/CircleWithMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleWithMarksApp.swift
+//  CircleWithMarks
+//
+//  Created by Max Franz Immelmann on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CircleWithMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
